06a2b03dc22c47671bc0c23a7a38da5ed116b5bf
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-03-04 11:17:13 +0100

    first commit

---
+++ Validators.Core/ContactValidator.cs
@@ -0,0 +1,13 @@
+﻿using Amica.vNext.Models;
+using FluentValidation;
+
+namespace Amica.vNext.Validation
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+		public ContactValidator()
+        {
+            RuleFor(contact => contact.Name).NotEmpty();
+        }
+    }
+}
