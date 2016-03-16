1fbb6b6177ec8f84e366f657010c7344e9354cad
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-03-16 17:34:03 +0100

    Validation for Vat objects.

---
+++ Validation.Tests/VatValidation.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Amica.vNext.Validation;
+using Amica.vNext.Models.ItalianPA;
+using FluentValidation.TestHelper;
+
+namespace Validation.Tests
+{
+    [TestFixture]
+    public class VatValidation
+    {
+        private VatValidator validator;
+
+		[SetUp]
+		public void Init()
+        {
+            validator = new VatValidator();
+        }
+
+		[Test]
+		public void Code()
+        {
+            validator.ShouldHaveValidationErrorFor(c => c.Code, string.Empty);
+            validator.ShouldHaveValidationErrorFor(c => c.Code, null as string);
+
+            validator.ShouldNotHaveValidationErrorFor(c => c.Code, "code");
+        }
+
+		[Test]
+		public void Name()
+        {
+            validator.ShouldHaveValidationErrorFor(c => c.Name, string.Empty);
+            validator.ShouldHaveValidationErrorFor(c => c.Name, null as string);
+
+            validator.ShouldNotHaveValidationErrorFor(c => c.Name, "name");
+        }
+		[Test]
+		public void PublicAdministrationNature()
+        {
+            validator.ShouldHaveValidationErrorFor(c => c.NaturaPA, new NaturaPA { Name = "hello" });
+
+            validator.ShouldNotHaveValidationErrorFor(c => c.NaturaPA, null as NaturaPA);
+            foreach (var n in PACollections.NaturaPA)
+                validator.ShouldNotHaveValidationErrorFor(c => c.NaturaPA, n);
+        }
+
+
+    }
+}
---
+++ Validators.Core/VatValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Amica.vNext.Models;
+using Amica.vNext.Models.ItalianPA;
+using FluentValidation;
+
+namespace Amica.vNext.Validation
+{
+    public class VatValidator : AbstractValidator<Vat>
+    {
+		public VatValidator()
+        {
+            RuleFor(vat => vat.Name).NotEmpty();
+            RuleFor(vat => vat.Code).NotEmpty();
+
+            RuleFor(vat => vat.NaturaPA)
+				.Must(BeValidNaturaPA).When(vat => vat.NaturaPA != null);
+        }
+
+		private static bool BeValidNaturaPA(NaturaPA challenge)
+        {
+            return PACollections.NaturaPA.Contains(challenge);
+        }
+    }
+}
