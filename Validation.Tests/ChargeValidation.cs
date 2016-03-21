0f346a9ac19b5afd3983c51f3b8c72fe475528de
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-03-21 16:21:39 +0100

    Validate Charge models

---
+++ Validation.Tests/ChargeValidation.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Amica.vNext.Validation;
+using Amica.vNext.Models.ItalianPA;
+using FluentValidation.TestHelper;
+using Amica.vNext.Models;
+
+namespace Validation.Tests
+{
+    [TestFixture]
+    public class ChargeValidation
+    {
+        private ChargeValidator validator;
+
+		[SetUp]
+		public void Init()
+        {
+            validator = new ChargeValidator();
+        }
+
+		[Test]
+		public void Name()
+        {
+            validator.ShouldHaveValidationErrorFor(c => c.Name, string.Empty);
+            validator.ShouldHaveValidationErrorFor(c => c.Name, null as string);
+
+            validator.ShouldNotHaveValidationErrorFor(c => c.Name, "name");
+        }
+		[Test]
+		public void Vat()
+        {
+            validator.ShouldHaveChildValidator(c => c.Vat, typeof(VatValidator));
+        }
+
+
+    }
+}
---
+++ Validators.Core/ChargeValidator.cs
@@ -0,0 +1,20 @@
+﻿using Amica.vNext.Models;
+using Amica.vNext.Models.ItalianPA;
+using FluentValidation;
+
+namespace Amica.vNext.Validation
+{
+    public class ChargeValidator : AbstractValidator<Charge>
+    {
+		public ChargeValidator()
+        {
+            RuleFor(charge => charge.Name).NotEmpty();
+            RuleFor(charge => charge.Vat).SetValidator(new VatValidator());
+        }
+
+		private static bool BeValidModalitaPagamentoPA(ModalitaPagamentoPA challenge)
+        {
+            return challenge.Code != null && PACollections.ModalitaPagamentoPA.ContainsKey(challenge.Code);
+        }
+    }
+}
