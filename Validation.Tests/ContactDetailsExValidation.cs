49f5fa592da053900d03047fce8432409611349a
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-05-17 16:51:25 +0200

    Add ContactDetailsExValidator class (with test)

---
+++ Validation.Tests/ContactDetailsExValidation.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Amica.Validation;
+using Amica.Models;
+
+namespace Validation.Tests
+{
+    [TestFixture]
+    public class ContactsDetailExValidation : BaseTestClass<ContactDetailsEx, ContactDetailsExValidator>
+    {
+        [Test]
+        public void NameIsRequired()
+        {
+            AssertRequired(c => c.Name);
+        }
+		[Test]
+		public void MailMustBeValid()
+        {
+            AssertValidEmail(c => c.Mail);
+        }
+		[Test]
+		public void PecMailMustBeValid()
+        {
+            AssertValidEmail(c => c.PecMail);
+        }
+
+    }
+}
---
+++ Validators/ContactDetailsExValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Amica.Models;
+
+namespace Amica.Validation
+{
+    public class ContactDetailsExValidator : AbstractValidator<ContactDetailsEx>
+    {
+		public ContactDetailsExValidator() 
+        {
+            RuleFor(address => address.Name).NotEmpty();
+            RuleFor(address => address.Mail).EmailAddress();
+            RuleFor(address => address.PecMail).EmailAddress();
+        }
+    }
+}
