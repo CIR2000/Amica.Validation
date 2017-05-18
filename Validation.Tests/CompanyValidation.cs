3451f0f1a26d0c86c611e8f813c1296e7ed791b8
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-05-18 09:20:33 +0200

    Add CompanyValidator

---
+++ Validation.Tests/CompanyValidation.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Amica.Validation;
+using FluentValidation.TestHelper;
+using Amica.Models;
+
+namespace Validation.Tests
+{
+    [TestFixture]
+    public class CompanyValidation : BaseTestClass<Company, CompanyValidator>
+    {
+		[Test]
+		public void NameIsRequired()
+        {
+            AssertRequired(c => c.Name);
+        }
+    }
+}
---
+++ Validators/CompanyValidator.cs
@@ -0,0 +1,14 @@
+﻿using Amica.Models;
+using FluentValidation;
+
+namespace Amica.Validation
+{
+    public class CompanyValidator : AbstractValidator<Company>
+    {
+		public CompanyValidator()
+        {
+            RuleFor(company => company.Name).NotEmpty();
+        }
+
+    }
+}
