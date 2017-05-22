978b43dd2b96870713271a62d6cfb2a8670a0692
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-05-22 11:24:57 +0200

    Add OrderReferenceValidator class

---
+++ Validation.Tests/OrderReferenceValidation.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Amica.Validation;
+using Amica.Models.Documents;
+using FluentValidation.TestHelper;
+
+namespace Validation.Tests
+{
+    [TestFixture]
+    public class OrderReferenceValidation : BaseTestClass<OrderReference, OrderReferenceValidator>
+    {
+		[Test]
+		public void ItemIdIsRequired()
+        {
+            AssertRequired(x => x.ItemId);
+        }
+		[Test]
+		public void NumberHasChildValidator()
+        {
+            validator.ShouldHaveChildValidator(x => x.Number, typeof(DocumentNumberValidator));
+        }
+    }
+}
---
+++ Validators/OrderReferenceValidator.cs
@@ -0,0 +1,14 @@
+﻿using Amica.Models.Documents;
+using FluentValidation;
+
+namespace Amica.Validation
+{
+    public class OrderReferenceValidator : AbstractValidator<OrderReference>
+    {
+		public OrderReferenceValidator()
+        {
+            RuleFor(fee => fee.Number).SetValidator(new DocumentNumberValidator());
+            RuleFor(fee => fee.ItemId).NotEmpty();
+        }
+    }
+}
