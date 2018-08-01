14607857f1960bdc038603f6b4c14b74944333ed
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-08-01 10:54:58 +0200

    Fix crash on validating Contact.PublicAdministrationIndex
    Closes CIR2000/Amica.Validation#2

--- Validation.Tests/ContactValidation.cs
+++ Validation.Tests/ContactValidation.cs
@@ -8,13 +8,13 @@ namespace Validation.Tests
     [TestClass]
     public class ContactValidation : BaseTestClass<Contact, ContactValidator>
     {
-		[TestMethod]
-		public void NameIsRequired()
+        [TestMethod]
+        public void NameIsRequired()
         {
-			AssertRequired(c => c.Name); 
+            AssertRequired(c => c.Name);
         }
-		[TestMethod]
-		public void TaxIdentificationNumberMustBeValid()
+        [TestMethod]
+        public void TaxIdentificationNumberMustBeValid()
         {
             AssertValidTaxIdentificationNumber(c => c.TaxIdentificationNumber);
         }
@@ -24,37 +24,41 @@ namespace Validation.Tests
             AssertValidVatIdentificationNumber(c => c.VatIdentificationNumber);
         }
 
-		[TestMethod]
-		public void PublicAdministrationIndexLength()
+        [TestMethod]
+        public void PublicAdministrationIndexLength()
         {
             const int length = 6;
-			validator.ShouldHaveValidationErrorFor(c => c.PublicAdministrationIndex, string.Empty);
-            validator.ShouldHaveValidationErrorFor(c => c.PublicAdministrationIndex, "A");
-            validator.ShouldHaveValidationErrorFor(c => c.PublicAdministrationIndex, new string('A', length+1));
-            validator.ShouldHaveValidationErrorFor(c => c.PublicAdministrationIndex, new string('A', length-1));
+            validator.ShouldHaveValidationErrorFor(c => c.PublicAdministrationCode, string.Empty);
+            validator.ShouldHaveValidationErrorFor(c => c.PublicAdministrationCode, "A");
+            validator.ShouldHaveValidationErrorFor(c => c.PublicAdministrationCode, new string('A', length + 1));
+            validator.ShouldHaveValidationErrorFor(c => c.PublicAdministrationCode, new string('A', length - 1));
 
-            validator.ShouldNotHaveValidationErrorFor(c => c.PublicAdministrationIndex, value:null);
-            validator.ShouldNotHaveValidationErrorFor(c => c.PublicAdministrationIndex, new string('A', length));
+            validator.ShouldNotHaveValidationErrorFor(c => c.PublicAdministrationCode, value: null);
+            validator.ShouldNotHaveValidationErrorFor(c => c.PublicAdministrationCode, new string('A', length));
         }
 
         [TestMethod]
-        public void ContactIsMustBeValid() {
-            validator.ShouldHaveValidationErrorFor(c => c.Relationship, new Relationship());
-
-            validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsClient = true });
-            validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsVendor = true });
-            validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsCourier = true });
+        public void ContactIsMustBeValid()
+        {
+            validator.ShouldHaveValidationErrorFor(c => c.Relationship, new Relationship() { IsActive = false, IsCompany = false });
+            validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsActive = true });
             validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsAgent = true });
             validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsAreaManager = true });
+            validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsClient = true });
+            validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsCompany = true });
+            validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsCourier = true });
+            validator.ShouldNotHaveValidationErrorFor(c => c.Relationship, new Relationship { IsVendor = true });
         }
 
-		[TestMethod]
-		public void AddressHasChildValidator() {
+        [TestMethod]
+        public void AddressHasChildValidator()
+        {
             validator.ShouldHaveChildValidator(c => c.Address, typeof(AddressExValidator));
         }
 
         [TestMethod]
-		public void OtherAddressesHasChildValidator() {
+        public void OtherAddressesHasChildValidator()
+        {
             validator.ShouldHaveChildValidator(c => c.OtherAddresses, typeof(ShippingAddressValidator));
         }
     }
--- Validation/ContactValidator.cs
+++ Validation/ContactValidator.cs
@@ -6,7 +6,7 @@ namespace Amica.Validation
 {
     public class ContactValidator : AbstractValidator<Contact>
     {
-		public ContactValidator()
+        public ContactValidator()
         {
             RuleFor(contact => contact.Name).NotEmpty();
 
@@ -14,15 +14,15 @@ namespace Amica.Validation
                 .Must(ValidatorHelpers.BeValidVatNumber)
                 .WithMessage(ErrorMessages.VatIdentificationNumber)
                 .When(contact => contact.VatIdentificationNumber != null);
-		    RuleFor(contact => contact.TaxIdentificationNumber)
+            RuleFor(contact => contact.TaxIdentificationNumber)
                 .Must(ValidatorHelpers.BeValidTaxIdNumber)
                 .WithMessage(ErrorMessages.TaxIdentificationNumber)
                 .When(c => c.TaxIdentificationNumber != null);
 
-            RuleFor(contact => contact.PublicAdministrationIndex).Length(6);
+            RuleFor(contact => contact.PublicAdministrationCode).Length(6);
 
             // TODO WithMessage, localized
-            RuleFor(contact => contact.Relationship).Must(BeValidContactKind);
+            RuleFor(contact => contact.Relationship).Must(BeValidRelationship);
 
             RuleFor(contact => contact.Address).SetValidator(new AddressExValidator());
 
@@ -30,15 +30,17 @@ namespace Amica.Validation
                 SetCollectionValidator(new ShippingAddressValidator());
         }
 
-		private static bool BeValidContactKind(Relationship relationship)
+        private static bool BeValidRelationship(Relationship relationship)
         {
             return !(
-                relationship.IsClient == false && 
-				relationship.IsVendor == false &&
-				relationship.IsCourier == false &&
-				relationship.IsAgent== false &&
-				relationship.IsAreaManager == false
-				);
+                relationship.IsActive == false &&
+                relationship.IsAgent == false &&
+                relationship.IsAreaManager == false &&
+                relationship.IsClient == false &&
+                relationship.IsCompany == false &&
+                relationship.IsCourier == false &&
+                relationship.IsVendor == false
+                );
         }
 
     }
