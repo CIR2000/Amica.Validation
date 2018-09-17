0c3ac10703f1d9cc6fd3e8ffc1e28fb726713830
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-09-17 14:40:08 +0200

    Add MailSettings and MailTempalte validator

---
+++ Validation.Tests/MailSettingsValidation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Amica.Validation;
+using FluentValidation.TestHelper;
+using Amica.Models;
+
+namespace Validation.Tests
+{
+    [TestClass]
+    public class MailSettingsValidation : BaseTestClass<MailSettings, MailSettingsValidator>
+    {
+		[TestMethod]
+		public void AddressHasChildValidator() {
+            validator.ShouldHaveChildValidator(c => c.Template, typeof(MailTemplateValidator));
+        }
+    }
+}
---
+++ Validation/MailSettingsValidator.cs
@@ -0,0 +1,13 @@
+﻿using Amica.Models;
+using FluentValidation;
+
+namespace Amica.Validation
+{
+    public class MailSettingsValidator : AbstractValidator<MailSettings>
+    {
+		public MailSettingsValidator()
+        {
+            RuleFor(x => x.Template).SetCollectionValidator(new MailTemplateValidator());
+        }
+    }
+}
