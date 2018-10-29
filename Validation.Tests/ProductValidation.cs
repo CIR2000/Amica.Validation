1541f1db5d9ba2fdb7284b16fd0bfcb832b0d02f
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-10-29 15:41:25 +0100

    Validation for Product class
    Addresses CIR2000/Amica.WebApp#29

---
+++ Validation.Tests/ProductValidation.cs
@@ -0,0 +1,53 @@
+﻿using Amica.Models;
+using Amica.Validation;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Validation.Tests {
+    [TestClass]
+    public class ProductValidation : BaseTestClass<Product, ProductValidator> {
+        [TestMethod]
+        public void NameAndDescriptionAreMutuallyRequired() {
+            challenge.Name = null;
+            challenge.Description = null;
+            validator.ShouldHaveValidationErrorFor(p => p.Name, challenge);
+            validator.ShouldHaveValidationErrorFor(p => p.Description, challenge);
+
+            challenge.Name = string.Empty;
+            challenge.Description = null;
+            validator.ShouldHaveValidationErrorFor(p => p.Name, challenge);
+            validator.ShouldHaveValidationErrorFor(p => p.Description, challenge);
+
+            challenge.Name = null;
+            challenge.Description = string.Empty;
+            validator.ShouldHaveValidationErrorFor(p => p.Name, challenge);
+            validator.ShouldHaveValidationErrorFor(p => p.Description, challenge);
+
+            challenge.Name = string.Empty;
+            challenge.Description = string.Empty;
+            validator.ShouldHaveValidationErrorFor(p => p.Name, challenge);
+            validator.ShouldHaveValidationErrorFor(p => p.Description, challenge);
+
+            challenge.Name = "name";
+            challenge.Description = null;
+            validator.ShouldNotHaveValidationErrorFor(p => p.Name, challenge);
+            validator.ShouldNotHaveValidationErrorFor(p => p.Description, challenge);
+
+            challenge.Name = "name";
+            challenge.Description = string.Empty;
+            validator.ShouldNotHaveValidationErrorFor(p => p.Name, challenge);
+            validator.ShouldNotHaveValidationErrorFor(p => p.Description, challenge);
+
+            challenge.Name = null;
+            challenge.Description = "description";
+            validator.ShouldNotHaveValidationErrorFor(p => p.Name, challenge);
+            validator.ShouldNotHaveValidationErrorFor(p => p.Description, challenge);
+
+            challenge.Name = string.Empty;
+            challenge.Description = "description";
+            validator.ShouldNotHaveValidationErrorFor(p => p.Name, challenge);
+            validator.ShouldNotHaveValidationErrorFor(p => p.Description, challenge);
+        }
+    }
+}
---
+++ Validation/ProductValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Amica.Models;
+using FluentValidation;
+
+namespace Amica.Validation {
+    public class ProductValidator : AbstractValidator<Product> {
+        public ProductValidator() {
+            RuleFor(product => product.Name)
+                .NotEmpty()
+                .When(product => string.IsNullOrEmpty(product.Description));
+
+            RuleFor(product => product.Description)
+                .NotEmpty()
+                .When(product => string.IsNullOrEmpty(product.Name));
+        }
+    }
+}
