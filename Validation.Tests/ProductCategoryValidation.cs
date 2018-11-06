b86b29bb9b781574d91e8a2c737dbb01645f3082
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-11-06 14:44:28 +0100

    New: ProductCategoryValidator class

---
+++ Validation.Tests/ProductCategoryValidation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Amica.Validation;
+using Amica.Models;
+
+namespace Validation.Tests
+{
+    [TestClass]
+    public class ProductCategoryValidation : BaseTestClass<ProductCategory, ProductCategoryValidator>
+    {
+        [TestMethod]
+        public void NameIsRequired()
+        {
+            AssertRequired(c => c.Name);
+        }
+    }
+}
---
+++ Validation/ProductCategoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using Amica.Models;
+using FluentValidation;
+
+namespace Amica.Validation
+{
+    public class ProductCategoryValidator : AbstractValidator<ProductCategory>
+    {
+        public ProductCategoryValidator()
+        {
+            RuleFor(warehouse => warehouse.Name).NotEmpty();
+        }
+    }
+}
